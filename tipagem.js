8709c97c1c0ff4ae0aeb8a8d81c74be31e446dd1
Jakson1996-hub <josejaksongouveia@gmail.com>
2022-08-16 18:04:34 -0300

    funções que fiz na ploomes

--- tipagem.js
+++ tipagem.js
@@ -2,6 +2,35 @@
 //untyped
 
 
-let minhaVar = 567;
-minhaVar= "texto";
-minhaVar= true;
+// let minhaVar = 567;
+// minhaVar= "texto";
+// minhaVar= true;
+
+
+
+// // // let preço = (10/100)*5
+// // // let prazo = (4/100)*2.5
+// // // let condicaoPagamento = (7/100)*1.25
+// // // let prefComtrafo = (5/100)*1.25
+
+// // // let chanceFechamento = preço+prazo+condicaoPagamento+prefComtrafo
+
+
+// // // console.log(chanceFechamento)
+
+
+let question1 = "false";
+let question2 = "false";
+let question3 = "true";
+let concatQuestions = (question1+question2+question3).length
+let curva;
+
+if (concatQuestions == 15){ curva =="A"}
+ 
+else if (concatQuestions == 14 )
+   {curva ==="B"}
+else if (concatQuestions == 13)
+   {curva == "C"}
+else
+{curva == ""}   
+console.log(curva)
