a6ebf6d54c7cbcec1fd9b9a4e6ee525ffe373907
Jakson1996-hub <josejaksongouveia@gmail.com>
2022-08-15 22:54:11 -0300

    learning javascript

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-console.log("rodando meu sistema de teste")
+// type number
+const meuNumero= 3;
+const numPrimeiro = 1;
+const numSegundo =2;
+
+const operacaoMatematica = numPrimeiro*meuNumero
+console.log(operacaoMatematica)
+
+
+//ponto flutuante
+
+const numeroPontoFlutuante = 3.3
+const numeroPontoFlutuanteComPonto = .5
+
+const novaOperacao = numPrimeiro*numeroPontoFlutuanteComPonto
+console.log(novaOperacao)
+
+//NaN
+
+//const string = 05
+//const Alura = string+jasajs
+//console.log(Alura)
+
+//
+
+const sayHello = "Hello"
+const sayHelloBrazil = sayHello+" Brazil"
+const sayHelloBrazilOnceAgain = sayHello+sayHelloBrazil
+
+console.log(sayHelloBrazilOnceAgain)
+
+// concatenação 
+
+const nome = "jakson "
+const secondName = "Gouveia" 
+
+console.log(nome+secondName)
